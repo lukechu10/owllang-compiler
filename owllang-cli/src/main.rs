faddbe830cf41dda54216ff12b37ed7afd9d1d92
Luke Chu <lukechu10@users.noreply.github.com>
2020-09-06 15:26:14 -0700

    feat(repl): add jit to repl

--- owllang-cli/src/main.rs
+++ owllang-cli/src/main.rs
@@ -1,53 +1,89 @@
 use clap::{App, Arg, ArgMatches};
+use llvm_sys::core::*;
+use llvm_sys::execution_engine::*;
+use llvm_sys::target::*;
+use llvm_sys::target_machine::*;
 use owllang_lexer::Lexer;
-use owllang_llvm_codegen::codegen_compilation_unit;
+use owllang_llvm_codegen::{c_str, codegen_compilation_unit, LlvmCodeGenVisitor};
 use owllang_parser::parser::Parser;
+use owllang_parser::Visitor;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
 
 fn repl_loop(matches: &ArgMatches) {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
+    unsafe {
+        let stdin = io::stdin();
+        let mut stdout = io::stdout();
 
-    loop {
-        let mut input = String::new();
-        print!("ready> ");
-        stdout.flush().unwrap();
+        let context = LLVMGetGlobalContext();
+        let mut module = LLVMModuleCreateWithNameInContext(c_str!("repl_tmp"), context);
+        let builder = LLVMCreateBuilderInContext(context);
 
-        let res = stdin.read_line(&mut input);
-        match res {
-            Ok(_) => {
-                if input == ".quit\n" {
-                    // exit repl
-                    println!("Exiting repl.");
-                    return;
-                }
+        LLVMLinkInMCJIT();
+        LLVM_InitializeNativeTarget();
+        LLVM_InitializeNativeAsmPrinter();
 
-                let mut lexer = Lexer::with_string(input.as_str());
-                // for token in &mut lexer {
-                //     println!("{:?}", token);
-                // }
-                let mut parser = Parser::new(&mut lexer);
-                let ast_result = parser.parse_compilation_unit();
-                match ast_result {
-                    Ok(ast) => {
-                        if matches.is_present("show-ast") {
-                            println!("{:#?}", ast);
-                        }
-                        match codegen_compilation_unit(&ast) {
-                            Err(err) => println!("{:?}", err),
-                            _ => {}
-                        }
+        let mut engine: LLVMExecutionEngineRef = std::ptr::null_mut();
+        let error: *mut *mut i8 = std::ptr::null_mut();
+        LLVMCreateExecutionEngineForModule(&mut engine, module, error);
+
+        let target_machine = LLVMGetExecutionEngineTargetMachine(engine);
+        let target_data_layout = LLVMCreateTargetDataLayout(target_machine);
+        LLVMSetModuleDataLayout(module, target_data_layout);
+
+        loop {
+            let mut input = String::new();
+            print!("ready> ");
+            stdout.flush().unwrap();
+
+            let res = stdin.read_line(&mut input);
+            match res {
+                Ok(_) => {
+                    if input == ".quit\n" {
+                        // exit repl
+                        println!("Exiting repl.");
+                        break;
                     }
-                    Err(err) => eprintln!(
-                        "Error at {}({}:{}). Message = {}",
-                        err.file_name, err.row, err.col, err.message
-                    ),
+
+                    let mut lexer = Lexer::with_string(input.as_str());
+                    let mut parser = Parser::new(&mut lexer);
+                    let ast_result = parser.parse_compilation_unit();
+
+                    match ast_result {
+                        Ok(ast) => {
+                            if matches.is_present("show-ast") {
+                                println!("{:#?}", ast);
+                            }
+                            // match codegen_compilation_unit(&ast) {
+                            //     Err(err) => println!("{:?}", err),
+                            //     _ => {}
+                            // }
+                            let mut codegen_visitor = LlvmCodeGenVisitor::new(module, builder);
+                            codegen_visitor.visit_compilation_unit(&ast).unwrap();
+                            let last_func = LLVMGetLastFunction(module);
+                            // LLVMDumpValue(last_func);
+                            LLVMDumpModule(module);
+
+                            LLVMAddModule(engine, module);
+                            let mut args: Vec<LLVMGenericValueRef> = Vec::new();
+                            let res = LLVMRunFunction(engine, last_func, 0, args.as_mut_ptr());
+                            println!("Evaluated to: {}", LLVMGenericValueToInt(res, false as i32));
+                            LLVMRemoveModule(engine, module, &mut module, error);
+                        }
+                        Err(err) => eprintln!(
+                            "Error at {}({}:{}). Message = {}",
+                            err.file_name, err.row, err.col, err.message
+                        ),
+                    }
                 }
+                Err(err) => println!("Error: {}", err),
             }
-            Err(err) => println!("Error: {}", err),
         }
+
+        LLVMDisposeModule(module);
+        LLVMDisposeBuilder(builder);
+        LLVMDisposeExecutionEngine(engine);
     }
 }
 
